443f75783175de17f5101496d985f53fbd081bee
unknown <Marlon Clingio@DESKTOP-NBJ0U4R.(none)>
2023-06-28 23:02:31 -0300

    Projeto Game Maria Jump

--- main.js
+++ main.js
@@ -1,34 +1,49 @@
 const mario = document.querySelector('.mario');
 const pipe = document.querySelector('.pipe');
 const gameOver = document.querySelector('.gameOver');
+const scoreElement = document.querySelector('.score');
+const highScoreElement = document.querySelector('.high-score');
+
+let score = 0;
+let highScore = localStorage.getItem("high-score") || 0;
+highScoreElement.innerText = `High Score: ${highScore}`;
 
 const jump = () => {
-    mario.classList.add('jump'); 
+    mario.classList.add('jump');
 
     setTimeout(() => {
         mario.classList.remove('jump');
     }, 500);
-    
+
 }
+
 const loop = setInterval(() => {
     const pipePosition = pipe.offsetLeft;
-    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px','');
+    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
 
-    if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80){
+    if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
         pipe.style.animation = 'none';
         pipe.style.left = `${pipePosition}px`;
- 
+
         mario.style.animation = 'none';
         mario.style.bottom = `${marioPosition}px`;
-     
+
         mario.src = "img/GameOver.png";
         mario.style.width = '65px';
         mario.style.marginLeft = '60px';
-        
+
         gameOver.style.opacity = '1';
 
         clearInterval(loop);
     }
-}, 10); 
+    else {
+        score++;
+        highScore = score >= highScore ? score : highScore;
+        localStorage.setItem("high-score", highScore);
+        scoreElement.innerText = `Score: ${score}`;
+        highScoreElement.innerText = `High Score: ${highScore}`;
+    }
+
+}, 10);
 
 document.addEventListener('click', jump);
